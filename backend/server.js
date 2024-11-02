ab3aeb428451d56ac4dc5d9477914d06d4c520be
Joshua Sol <joshuasol4950@gmail.com>
2024-11-02 13:36:38 +0800

    add post method to create product

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,23 @@ const app = express();
 
 app.use(express.json());
 
+app.post("/api/products", async (req, res) => {
+    const product = req.body;
 
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "Please provide all fields" });
+    }
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct });
+    } catch (err) {
+        console.error("Error in creating new Product: ", err.message)
+        res.status(500).json({ success: false, msg: "Server error" });
+    }
+});
 
 app.listen(5000, () => {
     connectDB();  // Connect to MongoDB
